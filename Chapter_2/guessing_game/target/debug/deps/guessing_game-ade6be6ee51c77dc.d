b0a18aed8f5e4b251a93bee3cc0d1ab7ff26da18
Hudson Ross <hudson@sejongconsortium.com>
2026-05-19 00:51:36 -0700

    Created basic beginnings of guessing game, where we prompt, store and display user input.

---
+++ Chapter_2/guessing_game/target/debug/deps/guessing_game-ade6be6ee51c77dc.d
@@ -0,0 +1,5 @@
+/Users/hudso/repos/Rust-Programming/rust-lang-book/Chapter_2/guessing_game/target/debug/deps/guessing_game-ade6be6ee51c77dc.d: src/main.rs
+
+/Users/hudso/repos/Rust-Programming/rust-lang-book/Chapter_2/guessing_game/target/debug/deps/guessing_game-ade6be6ee51c77dc: src/main.rs
+
+src/main.rs:
---
+++ Chapter_2/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/hudso/repos/Rust-Programming/rust-lang-book/Chapter_2/guessing_game/target/debug/guessing_game: /Users/hudso/repos/Rust-Programming/rust-lang-book/Chapter_2/guessing_game/src/main.rs
