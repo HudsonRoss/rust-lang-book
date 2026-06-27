36e4414e7f77b2795ffd4618442ee91e88c01f0b
Hudson Ross <hudson@sejongconsortium.com>
2026-06-27 12:43:17 -0700

    ...The MUT keyword prevents Type switching for a variable name...(does not compile).

--- Chapter_3/src/main.rs
+++ Chapter_3/src/main.rs
@@ -2,10 +2,10 @@ fn main() {
 
     //a further shadowing example
 
-    let spaces = "    ";
+    let mut spaces = "    ";
     let spaces = spaces.len();
 
     println!("The value of spaces is: {spaces}");
 
-    //Notice in this example, when we shadowed, we did NOT even keep the same TYPE of variable. Very cool. 
+    //Does not compile... the MUT keyword will prevent type switching...
 }
