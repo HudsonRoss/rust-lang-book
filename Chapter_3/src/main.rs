9ef11aa48d75f434690c9e5f81b17fe4b7a2c073
Hudson Ross <hudson@sejongconsortium.com>
2026-06-27 12:22:05 -0700

    Adding a constant. Lasts for the whole program in the scope in which it is declared.

--- Chapter_3/src/main.rs
+++ Chapter_3/src/main.rs
@@ -1,6 +1,14 @@
 fn main() {
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
     let mut x = 5;
+
+    let y = THREE_HOURS_IN_SECONDS;
+
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is {x}");    
+
+    println!("There are {y} seconds in 3 hours.");
 }
