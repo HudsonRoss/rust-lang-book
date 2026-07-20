9c0d463fa18593d844fc0205ff229e3cc4a28fb0
Hudson Ross <hudson@sejongconsortium.com>
2026-07-20 08:31:01 -0700

    Sect 3.3 - Statements

--- Chapter_3/src/main.rs
+++ Chapter_3/src/main.rs
@@ -1,7 +1,31 @@
 fn main() {
-    print_labeled_measurement(5, 'h');
+    /* Statements and Expressions:
+
+    STATEMENTS
+    -   Statements are instructions that perform some action and do not return a 
+        value.
+    -   Expressions evaluate to a resultant value.
+
+    ###Examples###
+
+    `let y = 6;` is a *statement*; it is performing the action of assignment, 
+    and so is:
+        `fn main() {
+            let y = 6; 
+        }`
+
+    Why? Neither bit of code returns a value, even though both of them are
+    doing something.
+
+
+    `let x = (let y = 6);`
+
+    The statement `let y = 6` does not return a value, so there is not to bind
+    x in the `let x =` fragment to. As a result, we will get compile time 
+    errors.
+
+    
+
+    */
 }
 
-fn print_labeled_measurement(value: i32, unit_label: char) {
-    println!("The measurement is: {value}{unit_label}");
-}
