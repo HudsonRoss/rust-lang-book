2585e9587b18cd5c252d15fe9cc8b8b98f8e34de
Hudson Ross <hudson@sejongconsortium.com>
2026-07-20 10:10:16 -0700

    Sect 3.3 - Expressions 2 - putting the block into its own function shows how it behaves, evaluates, and has no need for semicolons.

--- Chapter_3/src/main.rs
+++ Chapter_3/src/main.rs
@@ -13,10 +13,17 @@ fn main() {
     Consider the block below... 
     */
 
-    let y = {
-        let x = 3;  //statement, doesn't evaluate, needs semi-colon
-        x + 1       //note: No semi-colon here, this is fragment is an expression!
-    };              //this whole block does an assignment action, but returns
-                    // nothing. Hence, it is a statement, ending in a semicolon
+    let result = expression_x();
+
+    println!("The value of x is: {result}");
 }
 
+fn expression_x() -> i32 {
+    {
+        let x = 3;  //statement, doesn't evaluate, needs semi-colon
+        x + 1       //note: No semi-colon here, this is fragment is an expression!
+    }               //commenting out the assignment to y makes this a statement
+                    //so I should not need a semi-colon on line 19
+
+    //This expression compiles, and returns `4`.    
+}
