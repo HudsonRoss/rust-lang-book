ab022efe892579b4992640ae90f86743865d63fa
Hudson Ross <hudson@sejongconsortium.com>
2026-08-02 15:15:26 -0700

    Another example of a function whose only line of code is a Rust Expression (no semicolon needed).

--- Chapter_3/src/main.rs
+++ Chapter_3/src/main.rs
@@ -1,13 +1,9 @@
 fn main() {
-    let x = five();
+    let x = plus_one(5);
 
     println!("The value of x is: {x}");
 }
 
-fn five() -> i32 {
-    5
-
-    /* There are no function calls, macros, or even let statements in the five function—just the number 5 by itself. 
-    That’s a perfectly valid function in Rust. Note that the function’s return type is specified too, as -> i32. */
-    
+fn plus_one(x: i32) -> i32 {
+    x + 1 // <-- works even without the semicolon, but if we add a semicolon....
 }
