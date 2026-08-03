e52cad2f3725cb4070e913ae81f8de07040f40de
Hudson Ross <hudson@sejongconsortium.com>
2026-08-02 18:25:51 -0700

    Sect 3.5 - If with a let statement

--- Chapter_3/src/main.rs
+++ Chapter_3/src/main.rs
@@ -1,21 +1,11 @@
 fn main() {
-    let number = 6;
+    let condition = true;
+    let number = if condition {5} else {6};
 
-    if number % 4 == 0 {
-        println!("number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("number is divisible by 3");
-    } else if number % 2 == 0 {
-        println!("number is divisible by 2");
-    } else {
-        println!("number is not divisible by 4, 3, or 2");
-    }
+    println!("The value of number is: {number}");
+    
 }
 
 /* 
- 
-  each branch is evaluated top to bottom and the 1st matching condition
-  executes, in this case "else if number % 3 == 0 ". N.B., had that
-  branch not been there "else if number % 2 == 0" would have executed
-
+    let with an if . . . sorta like a ternary operator
  */
