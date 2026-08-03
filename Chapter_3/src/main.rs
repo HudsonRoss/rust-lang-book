1de6e81ed2d17d37adf845d73031685d8bf13880
Hudson Ross <hudson@sejongconsortium.com>
2026-08-02 18:46:33 -0700

    how to break from loops

--- Chapter_3/src/main.rs
+++ Chapter_3/src/main.rs
@@ -1,5 +1,14 @@
 fn main() {
-    loop {
-        println!("again");
-    }  
+    let mut counter = 0;
+    
+    let result = loop {
+        counter += 1; 
+
+        if counter == 10 {
+            break counter * 2 
+            //think of this as 'counter * 2, just before you break'
+        }
+    };
+
+    println!("The result is {result}");
 }
